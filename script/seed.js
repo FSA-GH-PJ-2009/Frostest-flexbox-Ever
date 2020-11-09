f176670c45406d34c3c2ef64d2f7a3d5f78f0b35
averyschiff <averyjschiff@gmail.com>
2020-11-09 13:48:37 -0800

    database defined

--- script/seed.js
+++ script/seed.js
@@ -7,6 +7,7 @@ async function seed() {
   await db.sync({force: true})
   console.log('db synced!')
 
+  /*
   const users = await Promise.all([
     User.create({email: 'cody@email.com', password: '123'}),
     User.create({email: 'murphy@email.com', password: '123'})
@@ -14,6 +15,7 @@ async function seed() {
 
   console.log(`seeded ${users.length} users`)
   console.log(`seeded successfully`)
+  */
 }
 
 // We've separated the `seed` function from the `runSeed` function.

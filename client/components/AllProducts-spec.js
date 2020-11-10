e4e89a49fb8cf5d310ebefdb4f14cbd0bb0e3a2c
HopeFourie <hopefourie@gmail.com>
2020-11-10 16:21:35 -0500

    began adding AllProduct Tests

--- client/components/AllProducts-spec.js
+++ client/components/AllProducts-spec.js
@@ -4,19 +4,19 @@ import {expect} from 'chai'
 import React from 'react'
 import enzyme, {shallow} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import {UserHome} from './user-home'
+import {AllProducts} from './AllProducts'
 
 const adapter = new Adapter()
 enzyme.configure({adapter})
 
-describe('UserHome', () => {
-  let userHome
+describe('AllProducts', () => {
+  let allProducts
 
   beforeEach(() => {
-    userHome = shallow(<UserHome email="cody@email.com" />)
+    allProducts = shallow(<AllProducts />)
   })
 
-  it('renders the email in an h3', () => {
-    expect(userHome.find('h3').text()).to.be.equal('Welcome, cody@email.com')
+  it('renders the All Noodles in an h1', () => {
+    expect(allProducts.find('h1').text()).to.be.equal('All Noodles')
   })
 })

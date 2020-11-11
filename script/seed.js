f0ad577f501812cc409e4a82c39e13bef5c8befc
Maria Chumachkova <maria@Marias-MacBook-Pro.local>
2020-11-11 16:37:13 -0500

    products display and actual data

--- script/seed.js
+++ script/seed.js
@@ -4,6 +4,98 @@ const db = require('../server/db')
 const {Product, User, Order} = require('../server/db/models')
 const faker = require('faker')
 
+const actualProducts = [
+  {
+    name: "MONK'S TAKE ON UDON",
+    price: 12,
+    inventory: 500,
+    description:
+      'Chicken, Udon noodles, fresno peppers, green onion, mushrooms, fresh basil',
+    imageUrl:
+      'https://www.pfchangs.com/images/default-source/menu/tokyo-udon-bowl.jpg?sfvrsn=c069fc35_4'
+  },
+  {
+    name: 'PAD THAI. JONESY PAD THAI.',
+    price: 11,
+    inventory: 200,
+    description:
+      'Rice noodles, Thai spices, tofu, green onion, peanuts. Substitute with Shrimp or Add extra Peanuts',
+    imageUrl:
+      'https://www.pfchangs.com/images/default-source/menu/chicken-pad-thai.jpg?sfvrsn=a269fc35_4'
+  },
+  {
+    name: 'SPICY MEETS SINGAPORE',
+    price: 14,
+    inventory: 230,
+    description:
+      'Thin rice noodles, light curry sauce, chicken, shrimp, onion, julienned vegetables',
+    imageUrl:
+      'https://www.pfchangs.com/images/default-source/menu/hokkien-street-noodles.jpg?sfvrsn=4668fc35_4'
+  },
+  {
+    name: "LO MEIN AND EVERYTHING THAT'S RIGHT ABOUT IT",
+    price: 12,
+    inventory: 150,
+    description:
+      'Egg noodles, mushrooms, Asian vegetables, savory soy sauce. Protein options: Vegetable, Chicken, Beef, Pork, Shrimp',
+    imageUrl:
+      'https://www.pfchangs.com/images/default-source/menu/signature-lo-mein.jpg?sfvrsn=5668fc35_4'
+  },
+  {
+    name: 'WOK AND ALL',
+    price: 10,
+    inventory: 190,
+    description:
+      'Wok-tossed with egg, carrots, bean sprouts, green onion. Protein options: Vegetable, Chicken, Beef, Pork, Shrimp',
+    imageUrl:
+      'https://paleoglutenfree.com/wp-content/uploads/2017/07/whole30-asain-noodle-bowl-1-900x642.jpg'
+  },
+  {
+    name: 'RAMEN WITH A KICK',
+    price: 16,
+    inventory: 400,
+    description:
+      'Spicy miso broth, shiitakes, carrots, togarashi bean sprouts, green onion',
+    imageUrl:
+      'https://www.pfchangs.com/images/default-source/default-album/spicy-miso-ramen.jpg?sfvrsn=46c2f835_4'
+  },
+  {
+    name: 'JUST WHAT NOODLE ORDERED',
+    price: 15,
+    inventory: 500,
+    description:
+      'Creamy tonkotsu pork broth, shiitakes, carrots, togarashi bean sprouts, green onion',
+    imageUrl:
+      'https://www.pfchangs.com/images/default-source/default-album/tonkotsu-ramen.jpg?sfvrsn=5ec2f835_2'
+  },
+  {
+    name: 'ZUCCINI AND CHILL(ed tofu)',
+    price: 11,
+    inventory: 200,
+    description:
+      'Zuccini noodles, marinated tofu, fresno peppers, green onion, mushrooms, fresh basil',
+    imageUrl:
+      'https://foodwithfeeling.com/wp-content/uploads/2016/02/Zucchini-Noodle-Ramen-Soup-6.jpg'
+  },
+  {
+    name: 'SESAME MUCHO',
+    price: 13,
+    inventory: 500,
+    description: 'Sesame seed sauce, fried noodles, green onion and mushrooms',
+    imageUrl:
+      'https://www.jocooks.com/wp-content/uploads/2020/04/sesame-noodles-1-11.jpg'
+  },
+  {
+    name: 'WHAT WE MEAN BY ROASTED',
+    price: 14,
+    inventory: 400,
+    description:
+      "Every noodle house has it's take on roasted veg and noodle. Well, this is ours.",
+    imageUrl:
+      'https://familystylefood.com/wp-content/uploads/2020/09/udon-ratatouille-500x375.jpg'
+  }
+]
+
 const productMaker = () => ({
   name: `${faker.commerce.productName()} Noodles`,
   description: faker.commerce.productDescription(),
@@ -56,6 +148,7 @@ populateSeeds()
 async function seed() {
   await db.sync({force: true})
   console.log('db synced!')
+  await Promise.all(actualProducts.map(product => Product.create(product)))
   await Promise.all(seedProducts.map(product => Product.create(product)))
   await Promise.all(seedUsers.map(user => User.create(user)))
   await Promise.all(seedOrder.map(order => Order.create(order)))

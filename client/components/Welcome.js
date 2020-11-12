ec4d78682635f524a9465184b9c9b06eac82be05
HopeFourie <hopefourie@gmail.com>
2020-11-12 13:00:07 -0500

    style: add styling to welcome page

--- client/components/Welcome.js
+++ client/components/Welcome.js
@@ -3,9 +3,10 @@ import React from 'react'
 export default function Welcome() {
   return (
     <div className="welcome">
-      <h1>Welcome to</h1>
+      <h1>
+        Welcome! <br /> Please Peruse our Noodles
+      </h1>
       <img className="welcome-img" src="./Logo.png" />
-      <h1> Please Peruse our Noodles!</h1>
     </div>
   )
 }

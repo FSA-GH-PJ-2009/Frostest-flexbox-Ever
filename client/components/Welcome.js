89162e7f806101aed8c525a4cc04b156c6e44a38
HopeFourie <hopefourie@gmail.com>
2020-11-12 15:42:05 -0500

    style: add style to welcome and auth form

--- client/components/Welcome.js
+++ client/components/Welcome.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default function Welcome() {
   return (
     <div className="welcome">
-      <h1>
-        Welcome! <br /> Please Peruse our Noodles
-      </h1>
+      <div className="welcome-text">
+        <h1>Welcome!</h1>
+        <Link className="peruse-button" to="/products">
+          Peruse Our Noodles
+        </Link>
+      </div>
       <img className="welcome-img" src="./Logo.png" />
     </div>
   )
